8fcc5679bac2e3df6274eeae13292ce909b2da96
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-04 06:06:16 +0200

    Update Build.sh

--- Rust/Build.sh
+++ Rust/Build.sh
@@ -3,3 +3,15 @@ set -euo pipefail
 
 git clone --depth=1 git@github.com:rust-lang/rust.git && cd rust
 ./configure --enable-llvm-link-shared --release-channel=nightly --enable-llvm-assertions --enable-offload --enable-enzyme --enable-clang --enable-lld --enable-option-checking --enable-ninja --disable-docs
+
+llvm-plugins
+llvm-enzyme
+llvm-assertions
+optimize-llvm
+new-symbol-mangling
+use-libcxx
+clang
+llvm-bitcode-linker
+lld
+optimize-tests
+use_bolt
