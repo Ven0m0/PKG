0326b846d93d1b0147cd4b854f72592fa8ffc7dc
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-04 23:38:23 +0200

    Create buils.sh

---
+++ Gitoxide/buils.sh
@@ -0,0 +1,6 @@
+
+
+
+cargo install gitoxide \
+  --no-default-features \
+  --features "fast,parallel,once_cell,walkdir,io-pipe,crc32,zlib-rs,gitoxide-core-async-client,lean-async,pack-cache-lru-dynamic"
