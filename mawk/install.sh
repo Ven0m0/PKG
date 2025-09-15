c2b8a66b42ea7626ea11f20a4f46d1fd055a4841
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-15 04:50:43 +0200

    Update install.sh

--- mawk/install.sh
+++ mawk/install.sh
@@ -2,4 +2,10 @@
 set -euo pipefail
 
 sudo pacman -S mawk
+sudo mkdir -p /usr/local/bin && sudo mkdir -p /usr/local/share/man/man1
 sudo ln -sfn /usr/bin/mawk /usr/local/bin/awk
+sudo ln -sfn /usr/share/man/man1/mawk.1.gz /usr/local/share/man/man1/awk.1
+
+# All
+#sudo ln -sfn /usr/bin/mawk /usr/bin/awk
+#sudo ln -sfn /usr/share/man/man1/mawk.1.gz /usr/share/man/man1p/awk.1p.gz
