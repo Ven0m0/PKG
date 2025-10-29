2f7160665dd9675a89690699c1eba8422671ddb0
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-29 21:38:25 +0100

    Update Build.sh

--- rust/Build.sh
+++ rust/Build.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 git clone --depth=1 git@github.com:rust-lang/rust.git && cd rust
-./configure --enable-llvm-link-shared --release-channel=nightly --enable-llvm-assertions --enable-offload --enable-enzyme --enable-clang --enable-lld --enable-option-checking --enable-ninja --disable-docs
+./configure --enable-llvm-link-shared --release-channel=nightly --enable-llvm-assertions --enable-offload --enable-enzyme --enable-clang --enable-lld \
+--enable-option-checking --enable-ninja --disable-docs --set llvm.libzstd=true --set rust.jemalloc --set rust.use-lld=true --set rust.lto=fat \
+--set rust.codegen-units=1 --disable-compiler-docs
 
 llvm-plugins
 llvm-enzyme
