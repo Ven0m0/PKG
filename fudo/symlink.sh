d9d1719d546bdd94da0ff1c319d95b53ffb87fe3
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-30 03:00:29 +0100

    Update symlink.sh

--- fudo/symlink.sh
+++ fudo/symlink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make the dir if it doesnt exist yet (it should be there by default however)
-[ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
-echo "Sudo earlier in path (at '~/.local/bin') as sudo to keep normal sudo installed"
-[ -f /usr/bin/fudo ] && ln -sf /usr/bin/fudo ~/.local/bin/sudo
+[ ! -d /usr/local/bin/ ] && mkdir -p /usr/local/bin/
+echo "Sudo earlier in path (at '/usr/local/bin/') as sudo to keep normal sudo installed"
+[ -f /usr/bin/fudo ] && ln -sf /usr/bin/fudo /usr/local/bin/sudo
 # Hide the command translation to stderr https://github.com/FragmentedCurve/fudo#using-fudo
 export FUDO_HIDE=1
