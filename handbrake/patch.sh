07042c6ec78a3042687f4b69609be98c2720a13c
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-02 08:28:34 +0100

    Update patch.sh

--- handbrake/patch.sh
+++ handbrake/patch.sh
@@ -12,54 +12,50 @@ The src_dir is the directory that contains the HandBrake source code (defaults t
 If no directory is found, the program exits"
 
 if [[ "$#" -gt 2 ]]; then
-    echo "$help"
-    exit 1
+  echo "$help"
+  exit 1
 fi
 for (( i=1; i <= "$#"; i++ )); do
-	case ${!i} in
-		-h | --help)
-			echo "$help"
-			exit 0
-			;;
-		-c | --clone)
-			if [[ "$i" -lt "$#" ]]; then
-				echo "$help"
-				exit 1
-			fi
-			rm -rf HandBrake
-			git clone https://github.com/HandBrake/HandBrake.git
-			;;
-		-*)
-			echo "${!i} option doesn't exist!"
-			echo "$help"
-			exit 1
-			;;
-		*)
-			src_dir="$1"
-			;;
-	esac
+  case ${!i} in
+    -h | --help)
+      echo "$help"
+      exit 0
+      ;;
+    -c | --clone)
+      if [[ "$i" -lt "$#" ]]; then
+        echo "$help"
+        exit 1
+      fi
+      rm -rf HandBrake
+      git clone https://github.com/HandBrake/HandBrake.git
+      ;;
+    -*)
+      echo "${!i} option doesn't exist!"
+      echo "$help"
+      exit 1
+      ;;
+    *)
+      src_dir="$1"
+      ;;
+  esac
 done
-if [[ ! -d "$src_dir" ]]; then
-	echo "Error: $src_dir directory doesn't exist!"
-	echo "$help"
-	exit 1
+if [[ !  -d "$src_dir" ]]; then
+  echo "Error: $src_dir directory doesn't exist!"
+  echo "$help"
+  exit 1
 fi
 
-if [[ ! -d "$BASEDIR/patches" ]]; then
-	echo "Error: patches directory doesn't exist!"
-	exit 1
+if [[ !  -d "$BASEDIR/patches" ]]; then
+  echo "Error: patches directory doesn't exist!"
+  exit 1
 fi
 
-for filename in "$BASEDIR"/patches/*.patch; do
-	if [[ ! -f "$filename" ]]; then
-		echo "Warning: No patch files found in $BASEDIR/patches/"
-		break
-	fi
-	echo "Applying patch: $(basename "$filename")"
-	patch -Np1 -t -d "$src_dir" < "$filename" || exit 1
-done
-
-# The flatpak build refers to the latest commit, so we add a commit that includes the patches
 cd "$src_dir" || exit 1
-git add .
+for filename in "$BASEDIR"/patches/*.patch; do
+  [[ !  -f "$filename" ]] && { echo "Warning: No patch files found in $BASEDIR/patches/"; break; }
+  echo "Applying patch: $(basename "$filename")"
+  git apply "$filename" || exit 1
+done
+
+git add . 
 git -c user.name='ven0m0' -c user.email='ven0m0' commit -m "Patch"
