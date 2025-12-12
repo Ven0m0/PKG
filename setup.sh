b4fdc3bda78aa361fdfec3283a2629e9338fb4a7
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-12 22:51:34 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+export DEPLOY_PIPEWIRE=1
+export DEPLOY_QT=1
+export URUNTIME_PRELOAD=1
+
+
+poststrip(){
+  strip -s -R .comment --strip-unneeded ./*.so*
+}
