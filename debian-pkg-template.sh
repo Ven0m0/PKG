24dc78c23749376c4d894c0d34c147ddb04903ed
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-20 18:20:58 +0100

    Update debian-pkg-template.sh

--- debian-pkg-template.sh
+++ debian-pkg-template.sh
@@ -2,17 +2,22 @@
 # shellcheck enable=all shell=bash source-path=SCRIPTDIR
 set -euo pipefail; shopt -s nullglob globstar
 IFS=$'\n\t' LC_ALL=C
+
+# From: https://github.com/PikaOS-Linux/pkg-pika-pbuilder
+
 has(){ command -v -- "$1" &>/dev/null; }
 APTCACHEHARDLINK=yes
+USENETWORK=yes
 export DEBIAN_FRONTEND="noninteractive"
-export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -w -DQT_NO_VERSION_TAGGING"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -w -DQT_NO_VERSION_TAGGING"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -w -DQT_NO_VERSION_TAGGING"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -w -DQT_NO_VERSION_TAGGING"
 export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DEB_BUILD_OPTIONS="parallel=8 nocheck notest terse"
 export DPKG_GENSYMBOLS_CHECK_LEVEL=0
-
+DEBBUILDOPTS="-j8 -nc --no-sign"
+EATMYDATA=yes
 # Clone Upstream
 mkdir -p ./src-pkg-name
 cp -rvf ./debian ./src-pkg-name/
