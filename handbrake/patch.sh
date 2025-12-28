efaa95f182f1e77ecffd3954d627b2b8d6503554
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-28 22:56:49 +0100

    Update patch.sh

--- handbrake/patch.sh
+++ handbrake/patch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck enable=all shell=bash source-path=SCRIPTDIR external-sources=true
+# shellcheck enable=all shell=bash source-path=SCRIPTDIR
 set -euo pipefail; shopt -s nullglob globstar
 IFS=$'\n\t' LC_ALL=C
 s=${BASH_SOURCE[0]}
@@ -15,46 +15,24 @@ The src_dir is the directory that contains the HandBrake source code (defaults t
 If no directory is found, the program exits'
 
 [[ $# -gt 2 ]] && { printf '%s\n' "$help"; exit 1; }
-
 for arg in "$@"; do
   case $arg in
   -h | --help)
-    printf '%s\n' "$help"
-    exit 0
-    ;;
-  -c | --clone)
-    rm -rf HandBrake
-    git clone https://github.com/HandBrake/HandBrake.git
-    ;;
+    printf '%s\n' "$help"; exit 0 ;;
+  -c | --clone) rm -rf HandBrake; git clone --depth 1 --filter=blob:none https://github.com/HandBrake/HandBrake.git ;;
   -*)
-    printf '%s option does not exist!\n%s\n' "$arg" "$help" >&2
-    exit 1
-    ;;
+    printf '%s option does not exist!\n%s\n' "$arg" "$help" >&2; exit 1 ;;
   *) src_dir=$arg ;;
   esac
 done
-
 [[ $src_dir == /* ]] || src_dir=$PWD/$src_dir
-
-[[ ! -d $src_dir ]] && {
-  printf 'Error: %s directory does not exist!\n%s\n' "$src_dir" "$help" >&2
-  exit 1
-}
-[[ ! -d $BASEDIR/patches ]] && {
-  printf 'Error: patches directory does not exist!\n' >&2
-  exit 1
-}
-
-builtin cd "$src_dir" || exit 1
-
+[[ ! -d $src_dir ]] && { printf 'Error: %s directory does not exist!\n%s\n' "$src_dir" "$help" >&2; exit 1; }
+[[ ! -d $BASEDIR/patches ]] && { printf 'Error: patches directory does not exist!\n' >&2; exit 1; }
+cd "$src_dir" || exit 1
 for filename in "$BASEDIR"/patches/*.patch; do
-  [[ ! -f $filename ]] && {
-    printf 'Warning: No patch files found in %s/patches/\n' "$BASEDIR" >&2
-    break
-  }
+  [[ ! -f $filename ]] && { printf 'Warning: No patch files found in %s/patches/\n' "$BASEDIR" >&2; break; }
   printf 'Applying patch: %s\n' "$(basename "$filename")"
   git apply "$filename" || exit 1
 done
-
 git add -A
 git -c user.name='ven0m0' -c user.email='ven0m0' commit -m "Patch"
