ee22384315e243a943372e1d251474482602f408
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-04 03:56:56 +0100

    Update legcord-launcher.sh

--- legcord/legcord-launcher.sh
+++ legcord/legcord-launcher.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-electron="/usr/bin/electron39"
-FLAGS="${XDG_CONFIG_HOME:-~/.config}/legcord-flags.conf"
-# Allow users to override command-line options
-if [[ -f "$FLAGS" ]]; then
-  USER_FLAGS="$(cat "$FLAGS")"
+if command -v electron39 &>/dev/null; then
+  electron="$(command -v electron39 2>/dev/null)"
+elif command -v electron38 &>/dev/null; then
+  electron="$(command -v electron38 2>/dev/null)"
+else
+  electron="/usr/bin/electron"
 fi
+FLAGS="${XDG_CONFIG_HOME:-${HOME}/.config}/legcord-flags.conf"
+# Allow users to override command-line options
+[[ -f "$FLAGS" ]] && USER_FLAGS="$(<"$FLAGS")"
 # shellcheck disable=SC2086 # USER_FLAGS has to be unquoted
 "$electron" /usr/share/legcord/app.asar $USER_FLAGS "$@"
