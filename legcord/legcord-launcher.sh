3fa3b709d508317ef091bf8cc1821c8625b9c803
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-04 03:49:43 +0100

    Update legcord-launcher.sh

--- legcord/legcord-launcher.sh
+++ legcord/legcord-launcher.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-electron=/usr/bin/electron
-
-CONFIG=${XDG_CONFIG_HOME:-~/.config}
-FLAGS="$CONFIG/legcord-flags.conf"
-
+electron="/usr/bin/electron39"
+FLAGS="${XDG_CONFIG_HOME:-~/.config}/legcord-flags.conf"
 # Allow users to override command-line options
-if [ -f "$FLAGS" ]; then
+if [[ -f "$FLAGS" ]]; then
   USER_FLAGS="$(cat "$FLAGS")"
 fi
 # shellcheck disable=SC2086 # USER_FLAGS has to be unquoted
