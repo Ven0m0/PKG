8821434bcc3f7587164a6f46e977d0fa162a80fe
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-04 04:48:32 +0100

    Update ffmpeg-mini.sh

--- ffmpeg/ffmpeg-mini.sh
+++ ffmpeg/ffmpeg-mini.sh
@@ -1,31 +1,21 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
 sed -i -e 's|-O2|-Oz|' /etc/makepkg.conf
-
 get-pkgbuild
 cd "$BUILD_DIR"
-
-# debloat package, remove x265 support and AV1 encoding support
+# debloat package, remove x265 support
 sed -i \
   -e '/x265/d' \
-  -e '/librav1e/d' \
-  -e '/--enable-libsvtav1/d' \
-  -e 's/--enable-vapoursynth/--enable-small/' \
   "$PKGBUILD"
-
 cat "$PKGBUILD"
-
 # Do not build if version does not match with upstream
 if check-upstream-version; then
   makepkg -fs --noconfirm --skippgpcheck
 else
   exit 0
 fi
-
 ls -la
 rm -fv ./*-docs-*.pkg.tar.* ./*-debug-*.pkg.tar.*
 mv -v ./"$PACKAGE"-*.pkg.tar."$EXT" ../"$PACKAGE"-mini-"$ARCH".pkg.tar."$EXT"
-
 echo "All done!"
